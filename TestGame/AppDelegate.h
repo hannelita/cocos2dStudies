4ecb5d204eff91d6b5bc1e1832c5ae330653ec23
hanneli <hannelita@gmail.com>
2013-09-30 18:01:54 -0300

    Initial Commit

---
+++ TestGame/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TestGame
+//
+//  Created by Hanneli Tavante on 9/30/13.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TestGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGame
+//
+//  Created by Hanneli Tavante on 9/30/13.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
